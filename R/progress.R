da5844437089e43e58e062b62566f59ab5c17d4e
Robert M Flight <r.flight79@gmail.com>
2018-01-30 16:46:22 -0500

    adding a simple function that takes care of updating and stopping the progress object

--- R/progress.R
+++ R/progress.R
@@ -176,3 +176,19 @@ show_time <- function(x) {
 }
 
 now <- function() proc.time()[[3]]
+
+#' updating progress bars
+#'
+#' Takes care of updating a progress bar and stopping when appropriate
+#'
+#' @param .pb the progress bar object
+#'
+#' @export
+#' @return the progress bar
+update_progress <- function(.pb = NULL) {
+  if ((!is.null(.pb)) && inherits(.pb, "Progress") && (.pb$i < .pb$n)) .pb$tick()$print()
+
+  if ((!is.null(.pb)) && inherits(.pb, "Progress") && (.pb$i == .pb$n)) .pb$stop()$print()
+
+  .pb
+}
